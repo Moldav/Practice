e75de0cc04551a149ead6ed169cf810a829c1f57
Moldav <dionisbolun@gmail.com>
2017-05-15 17:13:08 +0300

    Initial Commit

---
+++ Practice_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Practice_App.Startup))]
+namespace Practice_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
